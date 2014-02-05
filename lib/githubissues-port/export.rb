981b087ab3bdb11c6beca56bafec6f9c12639e23
Hemali Jain-TCS <Hemali_Jain-TCS@external.mckinsey.com>
2014-02-05 01:06:14 -0800

    reverting changes to check without milestone, wil it work

--- lib/githubissues-port/export.rb
+++ lib/githubissues-port/export.rb
@@ -4,49 +4,30 @@ require 'axlsx'
 module Githubissues
   module Port   
     class Githubissues::Port::Export
-      attr_reader :connection, :owner, :repo, :path
+      attr_reader :path, :fields
       def initialize connection, owner, repo, path, options = {}
         @path = path
-        @connection = connection
-        @owner = owner
-        @repo = repo
-        @default_fields = %w(number title body labels assignee  state milestone created_at closed_at comments comments_url events_url html_url labels_url) 
-        @fields = (options.has_key? :fields) ? options[:fields] : @default_fields 
-        generate_excel
-      end
-
-      def generate_excel
-        Axlsx::Package.new do |excel|
-          generate_sheet excel, 'open'
-          generate_sheet excel, 'closed'
-          excel.serialize path
-        end
-      end
-
-      def generate_sheet excel, state
-        excel.workbook.add_worksheet(:name => state) do |sheet|
-          sheet.add_row @fields        
-          issues = @connection.issues.list user: @owner,
-                                           repo: @repo,
-                                           filter: 'all',
-                                           auto_pagination: true,
-                                           state: state
-          issues.each{|issue| sheet.add_row generate_row(issue)}
-        end      
-      end
-
-      def generate_row issue
-        @fields.collect do |field|
-          case field.downcase
-            when 'assignee'
-              issue.assignee.login unless issue.assignee.nil?
-            when 'labels'
-              issue.labels.map(&:name).join(', ') unless issue.labels.nil?
-            when 'milestone'
-              issue.milestone.title unless issue.milestone.nil?
-            else
-              issue.send field
+        fields = (options.has_key? :fields) ? options[:fields] :  %w(number title body labels assignee  state milestone created_at closed_at comments comments_url events_url html_url labels_url)
+        issues = connection.issues.list user: owner, repo: repo, filter: 'all', auto_pagination: true          
+        Axlsx::Package.new do |p|
+          p.workbook.add_worksheet(:name => @repo) do |s|
+            s.add_row fields
+            issues.each do |i|
+              row = Array.new
+              fields.each do |f|
+                row.push  case f.downcase
+                            when 'assignee'
+                              i.assignee.login unless i.assignee.nil?
+                            when 'labels'
+                              i.labels.map(&:name).join(', ') unless i.labels.nil?
+                            else
+                              i.send f
+                          end
+              end unless issues.nil?
+              s.add_row row
+            end
           end
+          p.serialize path
         end
       end
     end
